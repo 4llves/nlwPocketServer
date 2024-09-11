698ed456169d7c2f62afedb56d9bc331e6be65a4
4llves <jhonatab4@gmail.com>
2024-09-11 20:16:30 -0300

    feat: Introduce goal creation endpoint

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,37 @@
 import fastify from 'fastify'
+import {
+  type ZodTypeProvider,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
 
-const app = fastify()
+import z from 'zod'
+import { createGoal } from '../functions/create-goal'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.post(
+  '/goals',
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number().int().min(1).max(7),
+      }),
+    },
+  },
+  async req => {
+    const { title, desiredWeeklyFrequency } = req.body
+
+    await createGoal({
+      title,
+      desiredWeeklyFrequency,
+    })
+  }
+)
 
 app
   .listen({
